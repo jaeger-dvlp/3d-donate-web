3b8c82c7606bc002d00046d0dfffc08a7c166931
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-04 23:45:47 +0300

    set app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Components/Header';
 
 function App() {
-  return <div className="app">3D Donate</div>;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
