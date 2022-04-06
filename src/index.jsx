ee9b04fb657490f32fd3262d06b14db7d9c23e6c
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-06 03:15:28 +0300

    set i18next

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
+import i18next from 'i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+
 import App from './App';
 
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('3d-donate'),
-);
+i18next
+  .use(HttpApi)
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    debug: false,
+    whitelist: ['en', 'tr'],
+    supportedLngs: ['en', 'tr'],
+    fallbackLng: 'en',
+    detection: {
+      order: ['path', 'htmlTag'],
+    },
+    backend: {
+      loadPath: '/assets/translations/{{lng}}/translation.json',
+    },
+    react: {
+      useSuspense: false,
+    },
+  })
+  .then(() => {
+    render(<App />, document.getElementById('3d-donate'));
+  });
