400cf464e605ab89b09afae98f5292da3765170b
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-06 22:40:54 +0300

    set header links

---
+++ src/Content.js
@@ -0,0 +1,36 @@
+const Contents = {
+  header: {
+    homepage: {
+      links: {
+        tr: '/tr/',
+        en: '/',
+      },
+    },
+    products: {
+      links: {
+        tr: '/tr/products/',
+        en: '/products/',
+      },
+    },
+    contact: {
+      links: {
+        tr: '/tr/contact/',
+        en: '/contact/',
+      },
+    },
+    whatarewedoing: {
+      links: {
+        tr: '/tr/what-are-we-doing/',
+        en: '/what-are-we-doing/',
+      },
+    },
+    account: {
+      links: {
+        tr: '/tr/account/',
+        en: '/account/',
+      },
+    },
+  },
+};
+
+export default Contents;
