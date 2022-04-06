46c85c5015a59ac1d334bac2a11ad89a9ca1f2cb
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-06 03:15:28 +0300

    import components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header';
+import Loader from './Components/Loader';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
     <Router>
+      <Loader />
       <Header />
+      <Switch>
+        <Route path={['/tr', '/en', '/']}>
+          <Homepage />
+        </Route>
+      </Switch>
     </Router>
   );
 }
