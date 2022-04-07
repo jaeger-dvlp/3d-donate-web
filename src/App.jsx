3a4b255e30d2ad5d1f1805b6584daa0151750c18
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-08 02:16:35 +0300

    Import cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Cart from './Components/Cart';
 import Header from './Components/Header';
 import Loader from './Components/Loader';
 import Homepage from './Pages/Homepage';
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Loader />
       <Header />
+      <Cart />
       <Switch>
         <Route path={['/tr', '/en', '/']}>
           <Homepage />
