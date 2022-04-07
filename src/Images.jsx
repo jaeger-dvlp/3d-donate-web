a1b7dc63922440ef67678c6fcde32dcb19b67766
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-08 01:38:38 +0300

    import & export images

--- src/Images.jsx
+++ src/Images.jsx
@@ -2,6 +2,16 @@ import Logo from './img/brand-logo.png';
 
 import Homebanner from './img/homepage/homebanner.svg';
 
-const Images = { Logo, Homebanner };
+import MockProduct1 from './img/products/mockproduct1.jpg';
+import MockProduct2 from './img/products/mockproduct2.jpg';
+import MockProduct3 from './img/products/mockproduct3.jpg';
+
+const Images = {
+  Logo,
+  Homebanner,
+  MockProduct1,
+  MockProduct2,
+  MockProduct3,
+};
 
 export default Images;
