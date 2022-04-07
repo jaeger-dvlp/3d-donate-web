abfd454c57f855e33acdb3e570a9191bf119c738
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-08 00:16:13 +0300

    get translated text

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { t } from 'i18next';
 import Images from '../Images';
 
 export default function Homepage() {
@@ -7,17 +8,11 @@ export default function Homepage() {
       <div className="font-pop p-5 pt-20 w-full flex flex-wrap justify-center content-center min-h-screen bg-gradient-to-r from-white to-red-100">
         <div className="w-full max-w-7xl grid gap-5 place-content-center place-items-center xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 grid-cols-1">
           <div className="w-full p-0 grid grid-cols-1 gap-5 place-content-center place-items-start">
-            <h1 className="xl:text-4xl lg:text-4xl text-3xl text-red-500 font-bold">
-              Kalp neredeyse,
-              <br />
-              Umut oradadır.
+            <h1 className="xl:text-4xl lg:text-4xl whitespace-pre-wrap text-3xl text-red-500 font-bold">
+              {t('pages.homepage.banner.header')}
             </h1>
-            <p className="text-zinc-500 font-medium xl:text-lg lg:text-lg text-md">
-              3DDonate.com,
-              <br />
-              Ergoterapist İsmail İşler ve Emre Akıncı PhD. tarafından 3D
-              yazıcılarla tamamen ücretsiz olarak dezavantajlı bireylere adaptif
-              ürünler üretmek için organize edildi.
+            <p className="text-zinc-500 font-medium whitespace-pre-wrap xl:text-lg lg:text-lg text-md">
+              {t('pages.homepage.banner.description')}
             </p>
           </div>
           <div className="grid grid-cols-1 place-content-center place-items-center">
