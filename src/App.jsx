72a828635b21033d1fc1cdf1c8985384f159dafe
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-08 04:02:33 +0300

    Import cart context

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Cart from './Components/Cart';
 import Header from './Components/Header';
 import Loader from './Components/Loader';
+import { CartContextProvider } from './Contexts/CartContext';
 import Homepage from './Pages/Homepage';
 
 function App() {
   return (
-    <Router>
-      <Loader />
-      <Header />
-      <Cart />
-      <Switch>
-        <Route path={['/tr', '/en', '/']}>
-          <Homepage />
-        </Route>
-      </Switch>
-    </Router>
+    <CartContextProvider>
+      <Router>
+        <Loader />
+        <Header />
+        <Cart />
+        <Switch>
+          <Route path={['/tr', '/en', '/']}>
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router>
+    </CartContextProvider>
   );
 }
 
