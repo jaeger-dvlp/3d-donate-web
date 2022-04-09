497495286f41ea768126219ab44859baecf8245f
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-10 02:17:57 +0300

    import & export images

--- src/Images.jsx
+++ src/Images.jsx
@@ -2,6 +2,10 @@ import Logo from './img/brand-logo.png';
 
 import Homebanner from './img/homepage/homebanner.svg';
 
+import SupportUs from './img/homepage/supportus.svg';
+import Cooperation from './img/homepage/cooperation.svg';
+import HowToSupport from './img/homepage/howtosupport.svg';
+
 import MockProduct1 from './img/products/mockproduct1.jpg';
 import MockProduct2 from './img/products/mockproduct2.jpg';
 import MockProduct3 from './img/products/mockproduct3.jpg';
@@ -9,6 +13,9 @@ import MockProduct3 from './img/products/mockproduct3.jpg';
 const Images = {
   Logo,
   Homebanner,
+  SupportUs,
+  Cooperation,
+  HowToSupport,
   MockProduct1,
   MockProduct2,
   MockProduct3,
