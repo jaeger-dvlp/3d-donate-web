f4a60b0a106ab57c5a68fb9bd7ffc94f91d8c922
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-10 06:02:36 +0300

    create page

---
+++ src/Pages/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Products() {
+  return (
+    <div className="w-full min-h-screen grid grid-cols-1 place-content-center place-items-center p-5">
+      <span>Products</span>
+    </div>
+  );
+}
