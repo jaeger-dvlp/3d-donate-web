004f1ef833ce3a60386b2efc4b8c1d00f5b06537
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-10 21:45:16 +0300

    create component & complete

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import i18next, { t } from 'i18next';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import CartContext from '../Contexts/CartContext';
+
+export default function ProductCard({ incomingProduct }) {
+  const { myCart, setMyCart } = useContext(CartContext);
+  const updateCart = (product) => {
+    if (!myCart) {
+      setMyCart(product);
+    } else {
+      alert('max length 1 update popup');
+    }
+  };
+
+  return (
+    <div className="w-full xl:max-w-lg lg:max-w-lg max-w-xs grid grid-cols-1 gap-4 transition-all duration-200 hover:shadow-lg hover:border-zinc-200 bg-white rounded-3xl shadow-md border border-zinc-100 place-content-start place-items-start p-5">
+      <Link
+        className="rounded-2xl overflow-hidden"
+        to={`/products/${incomingProduct.productSlug}`}
+      >
+        <img
+          className="w-full"
+          src={incomingProduct.productImage}
+          alt={incomingProduct.productTitle[i18next.language]}
+        />
+      </Link>
+
+      <h1 className="xl:text-2xl lg:text-2xl text-lg text-left text-zinc-400 font-semibold">
+        {incomingProduct.productTitle[i18next.language]}
+      </h1>
+      <div className="w-full flex flex-wrap justify-start content-center gap-5">
+        <button
+          type="button"
+          onClick={() => updateCart(incomingProduct)}
+          className="p-3 active:scale-75 font-semibold hover:bg-red-200 transition-all duration-200 rounded-lg w-full max-w-[8rem] bg-red-100 text-brand-red"
+        >
+          {t('buttons.add-to-cart')}
+        </button>
+        <Link
+          className="self-center text-brand-red hover:underline text-sm"
+          to={`/products/${incomingProduct.productSlug}`}
+        >
+          {t('buttons.see-details')}
+        </Link>
+      </div>
+    </div>
+  );
+}
