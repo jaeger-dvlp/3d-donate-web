ba4cc4877e80fcf47319457031410205a79d1f9a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-10 06:03:50 +0300

    import & export images

--- src/Images.jsx
+++ src/Images.jsx
@@ -10,6 +10,8 @@ import MockProduct1 from './img/products/mockproduct1.jpg';
 import MockProduct2 from './img/products/mockproduct2.jpg';
 import MockProduct3 from './img/products/mockproduct3.jpg';
 
+import page404 from './img/mix/404.svg';
+
 const Images = {
   Logo,
   Homebanner,
@@ -19,6 +21,7 @@ const Images = {
   MockProduct1,
   MockProduct2,
   MockProduct3,
+  page404,
 };
 
 export default Images;
