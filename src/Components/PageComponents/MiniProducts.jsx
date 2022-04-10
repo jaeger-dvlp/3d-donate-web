29845763cc44e48a14e3f75de336ff23dbd59379
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-10 21:45:17 +0300

    Render products with ProductCard component

--- src/Components/PageComponents/MiniProducts.jsx
+++ src/Components/PageComponents/MiniProducts.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/no-unstable-nested-components */
 import i18next, { t } from 'i18next';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Contents from '../../Content';
-import CartContext from '../../Contexts/CartContext';
 import Images from '../../Images';
+import ProductCard from '../ProductCard';
 
 export default function MiniProducts() {
-  const { myCart, setMyCart } = useContext(CartContext);
   const [products, setProducts] = useState(null);
   const mockProducts = [
     {
@@ -36,52 +35,16 @@ export default function MiniProducts() {
     },
   ];
 
-  const updateCart = (product) => {
-    if (!myCart) {
-      setMyCart(product);
-    }
-  };
-
   useEffect(() => {
     setProducts(
       <div className="w-full font-pop place-content-start place-items-center xl:max-w-full lg:max-w-full max-w-lg grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
         {mockProducts.map((product) => (
-          <div
-            key={`homeProduct-${Math.floor(Math.random() * 9999)}-${Math.floor(
+          <ProductCard
+            key={`product-${Math.floor(Math.random() * 9999)}-${Math.floor(
               Math.random() * 9999,
             )}`}
-            className="w-full xl:max-w-lg lg:max-w-lg max-w-xs grid grid-cols-1 gap-4 transition-all duration-200 hover:shadow-lg hover:border-zinc-200 bg-white rounded-3xl shadow-md border border-zinc-100 place-content-start place-items-start p-5"
-          >
-            <Link
-              className="rounded-2xl overflow-hidden"
-              to={`/products/${product.productSlug}`}
-            >
-              <img
-                className="w-full"
-                src={product.productImage}
-                alt={product.productTitle[i18next.language]}
-              />
-            </Link>
-
-            <h1 className="xl:text-2xl lg:text-2xl text-lg text-left text-zinc-400 font-semibold">
-              {product.productTitle[i18next.language]}
-            </h1>
-            <div className="w-full flex flex-wrap justify-start content-center gap-5">
-              <button
-                type="button"
-                onClick={() => updateCart(product)}
-                className="p-3 active:scale-75 font-semibold hover:bg-red-200 transition-all duration-200 rounded-lg w-full max-w-[8rem] bg-red-100 text-brand-red"
-              >
-                {t('buttons.add-to-cart')}
-              </button>
-              <Link
-                className="self-center text-brand-red hover:underline text-sm"
-                to={`/products/${product.productSlug}`}
-              >
-                {t('buttons.see-details')}
-              </Link>
-            </div>
-          </div>
+            incomingProduct={product}
+          />
         ))}
       </div>,
     );
