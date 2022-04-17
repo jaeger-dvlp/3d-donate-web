812630772a8069cac57b3167fd1e8f7f4ae0d79a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-17 09:21:08 +0300

    update style

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -10,31 +10,35 @@ export default function ProductCard({ incomingProduct }) {
     if (!myCart) {
       setMyCart(product);
     } else {
-      alert('max length 1 update popup');
+      // Activate popup etc.
     }
   };
 
   return (
-    <div className="w-full xl:max-w-lg lg:max-w-lg max-w-xs grid grid-cols-1 gap-4 transition-all duration-200 hover:shadow-lg hover:border-zinc-200 bg-white rounded-3xl shadow-md border border-zinc-100 place-content-start place-items-start p-5">
+    <div
+      className="w-full group hover:bg-zinc-50 xl:max-w-lg lg:max-w-lg max-w-xs
+      grid grid-cols-1 gap-4 transition-all duration-200 hover:shadow-lg hover:border-zinc-200
+    bg-white rounded-3xl shadow-md border border-zinc-100 place-content-start place-items-start p-5"
+    >
       <Link
-        className="rounded-2xl overflow-hidden"
+        className="rounded-2xl border border-zinc/50 overflow-hidden"
         to={`/products/${incomingProduct.productSlug}`}
       >
         <img
-          className="w-full"
+          className="w-full group-hover:scale-110 transition-all duration-300"
           src={incomingProduct.productImage}
           alt={incomingProduct.productTitle[i18next.language]}
         />
       </Link>
 
-      <h1 className="xl:text-2xl lg:text-2xl text-lg text-left text-zinc-400 font-semibold">
+      <h1 className="xl:text-2xl w-full max-w-[14rem] whitespace-nowrap overflow-hidden lg:text-2xl text-lg text-left text-zinc-400 font-semibold">
         {incomingProduct.productTitle[i18next.language]}
       </h1>
       <div className="w-full flex flex-wrap justify-start content-center gap-5">
         <button
           type="button"
           onClick={() => updateCart(incomingProduct)}
-          className="p-3 active:scale-90 font-semibold hover:bg-brand-red/20 transition-all duration-200 rounded-lg w-full max-w-[8rem] bg-brand-red/10 text-brand-red"
+          className="p-3 focus:ring-2 ring-brand-red/30  active:scale-90 font-semibold hover:bg-brand-red/20 transition-all duration-200 rounded-lg w-full max-w-[8rem] border border-brand-red/50 bg-white text-brand-red"
         >
           {t('buttons.add-to-cart')}
         </button>
