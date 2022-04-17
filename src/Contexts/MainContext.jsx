5ed73dce810a8ad2ffdb0d24483ffd1f5cbcf53c
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-18 02:19:28 +0300

    crate states of popup

--- src/Contexts/MainContext.jsx
+++ src/Contexts/MainContext.jsx
@@ -5,8 +5,18 @@ const MainContext = createContext();
 
 export function MainContextProvider({ children }) {
   const [myCart, setMyCart] = useState(null);
+  const [popup, setPopup] = useState({
+    isVisible: false,
+    status: 'error',
+    text: '',
+  });
 
-  const value = useMemo(() => ({ myCart, setMyCart }));
+  const value = useMemo(() => ({
+    myCart,
+    setMyCart,
+    popup,
+    setPopup,
+  }));
 
   return <MainContext.Provider value={value}>{children}</MainContext.Provider>;
 }
