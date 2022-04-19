ee689c0032c5e7fe23b0d60114e8fbc0bc83af36
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-19 22:59:36 +0300

    set top padding

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -5,8 +5,9 @@ export default function Products() {
   const [searchVal, setSearchVal] = useState('');
 
   return (
-    <div className="w-full font-pop xl:pt-36 lg:pt-36 pt-24 p-5 grid grid-cols-1 place-content-start place-items-center">
+    <div className="w-full font-pop xl:pt-36 lg:pt-36 pt-28 p-5 grid grid-cols-1 place-content-start place-items-center">
       <ProductFilter props={{ searchVal, setSearchVal }} />
+      <div className="min-h-screen" />
     </div>
   );
 }
