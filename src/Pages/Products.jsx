b0641324bfd6b151e24dbc5a27168d9d524cb6de
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-19 22:25:56 +0300

    import search bar component

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ProductSearchBar from '../Components/PageComponents/ProductSearchBar';
 
 export default function Products() {
+  const [searchVal, setSearchVal] = useState('');
+
   return (
-    <div className="w-full min-h-screen grid grid-cols-1 place-content-center place-items-center p-5">
-      <span>Products</span>
+    <div className="w-full font-pop xl:pt-36 lg:pt-36 pt-24 p-5 grid grid-cols-1 place-content-start place-items-center">
+      <ProductSearchBar props={{ searchVal, setSearchVal }} />
     </div>
   );
 }
