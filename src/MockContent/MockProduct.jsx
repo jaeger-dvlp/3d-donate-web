298fa2be743fd09399cf5e949383fae94ae9fcc1
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-04-22 02:14:04 +0300

    create mock contents

---
+++ src/MockContent/MockProduct.jsx
@@ -0,0 +1,79 @@
+import Images from '../Images';
+
+const MockProducts = [
+  {
+    productTitle: {
+      tr: 'Adaptif Kaşık Tut.',
+      en: 'Adaptive Spoon H',
+    },
+    productSlug: 'adaptive-spoon-holder',
+    productImage: Images.MockProduct1.toString(),
+    modelId: 'STLOBJ001',
+  },
+  {
+    productTitle: {
+      tr: 'Adaptif Kalem Tut.',
+      en: 'Adaptive Pencil H.',
+    },
+    productSlug: 'adaptive-pencil-holder',
+    productImage: Images.MockProduct2.toString(),
+    modelId: 'STLOBJ002',
+  },
+  {
+    productTitle: {
+      tr: 'Adaptif Kapak',
+      en: 'Adaptive Cover',
+    },
+    productSlug: 'adaptive-cover',
+    productImage: null,
+    modelId: 'STLOBJ003',
+  },
+  {
+    productTitle: {
+      tr: 'Çoklu Poşet Tutucu',
+      en: 'Multi Bag Holder',
+    },
+    productSlug: 'multi-bag-holder',
+    productImage: null,
+    modelId: 'STLOBJ004',
+  },
+  {
+    productTitle: {
+      tr: 'Konserve Açacağı',
+      en: 'Can Opener',
+    },
+    productSlug: 'can-opener',
+    productImage: null,
+    modelId: 'STLOBJ005',
+  },
+  {
+    productTitle: {
+      tr: 'Adaptif Anahtar',
+      en: 'Adaptive Switch',
+    },
+    productSlug: 'adaptive-switch',
+    productImage: null,
+    modelId: 'STLOBJ006',
+  },
+
+  {
+    productTitle: {
+      tr: 'Adaptif Fermuar',
+      en: 'Adaptive Zipper',
+    },
+    productSlug: 'adaptive-zipper',
+    productImage: null,
+    modelId: 'STLOBJ007',
+  },
+  {
+    productTitle: {
+      tr: 'Parmak Destekli Anahtarlık',
+      en: 'Finger Support Keychain',
+    },
+    productSlug: 'finger-support-keychain',
+    productImage: null,
+    modelId: 'STLOBJ008',
+  },
+];
+
+export default MockProducts;
