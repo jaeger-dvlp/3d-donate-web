d93972031a55fcbce694d4fa38680f400eee360e
JAEGER <omer.kayalar0101@icloud.com>
2022-05-12 13:32:25 +0300

    feat(products.jsx): Filter products and return. if no products found, return noproducts component

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,21 +1,53 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable arrow-body-style */
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable operator-linebreak */
+import i18next from 'i18next';
 import React, { useState } from 'react';
+import NoProducts from '../Components/PageComponents/NoProducts';
 import ProductFilter from '../Components/PageComponents/ProductFilter';
 import ProductCard from '../Components/ProductCard';
 import MockProducts from '../MockContent/MockProduct';
 
 export default function Products() {
-  const [searchVal, setSearchVal] = useState('');
+  const [searchVal, setSearchVal] = useState(null);
 
-  const [allProducts] = useState(
-    MockProducts.map((product) => <ProductCard incomingProduct={product} />),
-  );
+  const AllProducts = () => {
+    if (searchVal) {
+      if (
+        MockProducts.filter(({ productTitle }) =>
+          productTitle[i18next.language]
+            .toLowerCase()
+            .includes(searchVal.toLowerCase()),
+        ).length > 0
+      ) {
+        return MockProducts.map((product) => {
+          if (
+            product.productTitle[i18next.language]
+              .toLowerCase()
+              .includes(searchVal.toLowerCase())
+          ) {
+            return <ProductCard incomingProduct={product} />;
+          }
+        });
+      }
+      return <NoProducts />;
+    }
+    return MockProducts.map((product) => {
+      return <ProductCard incomingProduct={product} />;
+    });
+  };
 
   return (
     <div className="w-full font-pop xl:pt-36 gap-14 lg:pt-36 pt-28 p-5 grid grid-cols-1 place-content-start place-items-center">
       <ProductFilter props={{ searchVal, setSearchVal }} />
       <div className="w-full max-w-7xl grid grid-cols-1 place-content-start place-items-center">
         <div className="w-full relative font-pop place-content-start place-items-center max-w-full grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
-          {allProducts}
+          <AllProducts />
         </div>
       </div>
     </div>
