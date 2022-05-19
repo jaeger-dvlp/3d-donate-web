4a133c34ea69104bd9189ea3630c578cc561537f
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-05-19 12:22:30 +0300

    create as draft

---
+++ src/Pages/ProductPage.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable operator-linebreak */
+import React from 'react';
+import i18next, { t } from 'i18next';
+import CartContext from '../Contexts/MainContext';
+import NoProducts from '../Components/PageComponents/NoProducts';
+import MockProducts from '../MockContent/MockProduct';
+import Loader from '../Components/Loader';
+
+export default function ProductPage() {
+  return (
+    <div className="min-h-[70vh] xl:pt-36 lg:pt-36 pt-28 w-full flex flex-wrap justify-center content-start p-0 m-0">
+      TEST
+    </div>
+  );
+}
