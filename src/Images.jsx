5c03b0afc300cda18638c0a8c98d5c36432d9f3d
JAEGER <omer.kayalar0101@icloud.com>
2022-05-20 17:54:04 +0300

    import & export images

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,14 +1,17 @@
 import Logo from './img/brand-logo.png';
 
-import Homebanner from './img/homepage/homebanner.svg';
+import Homebanner from './img/homepage/homebanner.jpg';
+import Galata from './img/homepage/galata.jpg';
 
-import SupportUs from './img/homepage/supportus.svg';
-import Cooperation from './img/homepage/cooperation.svg';
-import HowToSupport from './img/homepage/howtosupport.svg';
+import SupportUs from './img/homepage/supportus.jpg';
+import Cooperation from './img/homepage/cooperation.jpg';
+import HowToSupport from './img/homepage/howtosupport.jpg';
 
 import MockProduct1 from './img/products/mockproduct1.jpg';
 import MockProduct2 from './img/products/mockproduct2.jpg';
 import MockProduct3 from './img/products/mockproduct3.jpg';
+import EducationSet1 from './img/products/education-set-1.jpg';
+import BirdHouse from './img/products/bird-house.jpg';
 
 import page404 from './img/mix/404.svg';
 import placeholder from './img/mix/placeholder.svg';
@@ -16,12 +19,15 @@ import placeholder from './img/mix/placeholder.svg';
 const Images = {
   Logo,
   Homebanner,
+  Galata,
   SupportUs,
   Cooperation,
   HowToSupport,
   MockProduct1,
   MockProduct2,
   MockProduct3,
+  EducationSet1,
+  BirdHouse,
   page404,
   placeholder,
 };
