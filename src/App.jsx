b03f3bf050e915606fbd868419fc9ca6981e0e58
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-05-22 21:18:11 +0300

    feat: set routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import Header from './Components/Header';
 import Loader from './Components/Loader';
 import Popup from './Components/Popup';
 import { MainContextProvider } from './Contexts/MainContext';
+import Blog from './Pages/Blog';
 import Homepage from './Pages/Homepage';
 import NotFound from './Pages/NotFound';
+import PostPage from './Pages/PostPage';
 import ProductPage from './Pages/ProductPage';
 import Products from './Pages/Products';
 
@@ -23,11 +25,13 @@ function App() {
         <Switch>
           <Route exact path={lang} component={Homepage} />
           <Route exact path={`${lang}/products`} component={Products} />
+          <Route exact path={`${lang}/blog`} component={Blog} />
           <Route
             exact
             path={`${lang}/products/:pSlug`}
             component={ProductPage}
           />
+          <Route exact path={`${lang}/blog/:postSlug`} component={PostPage} />
           <Route exact path={`${lang}/*`} component={NotFound} />
         </Switch>
         <Footer />
