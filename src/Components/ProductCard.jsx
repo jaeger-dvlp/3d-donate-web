799689c84009115a453ac0e4b341e218b1a83558
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-05-25 00:36:44 +0300

    feat: return categories of product

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -29,14 +29,22 @@ export default function ProductCard({ incomingProduct }) {
     bg-white rounded-xl overflow-hidden border border-zinc-100 place-content-start place-items-start p-0"
     >
       <Link
-        className="overflow-hidden w-full h-full border-b border-b-zinc-200"
-        to={`/${i18next.language}/products/${incomingProduct.productSlug}`}
+        className="overflow-hidden relative w-full h-full border-b border-b-zinc-200"
+        to={`/${i18next.language}/products/${incomingProduct.slug}`}
       >
-        {incomingProduct.productImage ? (
+        <div className="absolute left-2.5 top-2.5 flex flex-wrap justify-start content-start gap-2">
+          {incomingProduct.categories.map((category) => (
+            <span className="py-1 px-3 bg-brand-red shadow-md text-xs rounded-lg text-white">
+              {category[i18next.language]}
+            </span>
+          ))}
+        </div>
+
+        {incomingProduct.image ? (
           <img
             className="w-full object-cover h-[15rem]"
-            src={incomingProduct.productImage}
-            alt={incomingProduct.productTitle[i18next.language]}
+            src={incomingProduct.image}
+            alt={incomingProduct.slug}
           />
         ) : (
           <div className="w-full h-[15rem] grid grid-cols-1 place-content-center place-items-center">
@@ -46,13 +54,10 @@ export default function ProductCard({ incomingProduct }) {
       </Link>
 
       <div className="w-full grid grid-cols-1 place-content-start place-items-start p-5 pt-0 gap-2">
-        <h1 className="xl:text-xl w-full max-w-[14rem] whitespace-nowrap overflow-hidden lg:text-xl text-lg text-left text-gray-400 font-semibold">
-          {incomingProduct.productTitle[i18next.language].length > 19
-            ? `${incomingProduct.productTitle[i18next.language].substring(
-                0,
-                19,
-              )}...`
-            : incomingProduct.productTitle[i18next.language]}
+        <h1 className="xl:text-xl w-full  whitespace-nowrap overflow-hidden lg:text-xl text-lg text-left text-gray-400 font-semibold">
+          {incomingProduct.title[i18next.language].length > 15
+            ? `${incomingProduct.title[i18next.language].substring(0, 19)}..`
+            : incomingProduct.title[i18next.language]}
         </h1>
         <div className="w-full pt-0 flex flex-wrap justify-start content-center gap-5">
           <button
@@ -64,7 +69,7 @@ export default function ProductCard({ incomingProduct }) {
           </button>
           <Link
             className="self-center text-brand-red hover:underline text-xs"
-            to={`/products/${incomingProduct.productSlug}`}
+            to={`/products/${incomingProduct.slug}`}
           >
             {t('buttons.see-details')}
           </Link>
