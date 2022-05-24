8f4a881e8293535812993a481c5bdf36b00e4cea
jaeger-dvlp <omer.kayalar0101@icloud.com>
2022-05-25 00:35:04 +0300

    feat: export new images

--- src/Images.jsx
+++ src/Images.jsx
@@ -7,14 +7,56 @@ import SupportUs from './img/homepage/supportus.jpg';
 import Cooperation from './img/homepage/cooperation.jpg';
 import HowToSupport from './img/homepage/howtosupport.jpg';
 
-import MockProduct1 from './img/products/mockproduct1.jpg';
-import MockProduct2 from './img/products/mockproduct2.jpg';
-import MockProduct3 from './img/products/mockproduct3.jpg';
-import EducationSet1 from './img/products/education-set-1.jpg';
-import BirdHouse from './img/products/bird-house.jpg';
-
 import page404 from './img/mix/404.svg';
 import placeholder from './img/mix/placeholder.svg';
+
+/* Product Images */
+import animalCell from './img/products/animalCell.webp';
+import humanSkull from './img/products/humanSkull.webp';
+import turkeyMap from './img/products/turkeyMap.webp';
+import waterTransport from './img/products/waterTransport.webp';
+import avocadoGrowing from './img/products/avocadoGrowing.webp';
+import balloonHelicopter from './img/products/balloonHelicopter.webp';
+import automaticPlantWatering from './img/products/automaticPlantWatering.webp';
+import growingPlantsBottle from './img/products/growingPlantsBottle.webp';
+import sunClock from './img/products/sunClock.webp';
+import physicsShadows from './img/products/physicsShadows.webp';
+import geometricalShapes from './img/products/geometricalShapes.webp';
+import measuringTools from './img/products/measuringTools.webp';
+import wheeledSystems from './img/products/wheeledSystems.webp';
+import fractions from './img/products/fractions.webp';
+import additionSubtractionGames from './img/products/additionSubtractionGames.webp';
+import mathGeometryShapes from './img/products/mathGeometryShapes.webp';
+import periodicTable from './img/products/periodicTable.webp';
+import thumbPenHolder from './img/products/thumbPenHolder.webp';
+import paintingApparatus from './img/products/paintingApparatus.webp';
+import forkSpoonHolder from './img/products/forkSpoonHolder.webp';
+import multifunctionalApparatus from './img/products/multifunctionalApparatus.webp';
+import chessForVisuallyImpaired from './img/products/chessForVisuallyImpaired.webp';
+import cardboardCupHolder from './img/products/cardboardCupHolder.webp';
+import forkSpoonHolderWithGrip from './img/products/forkSpoonHolderWithGrip.webp';
+import gameConsoleApparatus from './img/products/gameConsoleApparatus.webp';
+import petBottleOpener from './img/products/petBottleOpener.webp';
+import oneFingerPaintBrushHolder from './img/products/oneFingerPaintBrushHolder.webp';
+import nailClipperApparatus from './img/products/nailClipperApparatus.webp';
+import roundPenHolderApparatus from './img/products/roundPenHolderApparatus.webp';
+import horse from './img/products/horse.webp';
+import fish from './img/products/fish.webp';
+import balanceToy from './img/products/balanceToy.webp';
+import dinosaur from './img/products/dinosaur.webp';
+import educationalToy from './img/products/educationalToy.webp';
+import benchyShip from './img/products/benchyShip.webp';
+import snowballMaker from './img/products/snowballMaker.webp';
+import shark from './img/products/shark.webp';
+import frog from './img/products/frog.webp';
+import legoToyParts from './img/products/legoToyParts.webp';
+import toyScissors from './img/products/toyScissors.webp';
+import raptor from './img/products/raptor.webp';
+import tangramGame from './img/products/tangramGame.webp';
+import snake from './img/products/snake.webp';
+import birdHouse from './img/products/birdHouse.webp';
+import birdFoodContainer from './img/products/birdFoodContainer.webp';
+import automaticWaterCup from './img/products/automaticWaterCup.webp';
 
 const Images = {
   Logo,
@@ -23,13 +65,56 @@ const Images = {
   SupportUs,
   Cooperation,
   HowToSupport,
-  MockProduct1,
-  MockProduct2,
-  MockProduct3,
-  EducationSet1,
-  BirdHouse,
   page404,
   placeholder,
+  productsImages: {
+    animalCell,
+    humanSkull,
+    turkeyMap,
+    waterTransport,
+    avocadoGrowing,
+    balloonHelicopter,
+    automaticPlantWatering,
+    growingPlantsBottle,
+    sunClock,
+    physicsShadows,
+    geometricalShapes,
+    measuringTools,
+    wheeledSystems,
+    fractions,
+    additionSubtractionGames,
+    mathGeometryShapes,
+    periodicTable,
+    thumbPenHolder,
+    paintingApparatus,
+    forkSpoonHolder,
+    multifunctionalApparatus,
+    chessForVisuallyImpaired,
+    cardboardCupHolder,
+    forkSpoonHolderWithGrip,
+    gameConsoleApparatus,
+    petBottleOpener,
+    oneFingerPaintBrushHolder,
+    nailClipperApparatus,
+    roundPenHolderApparatus,
+    horse,
+    fish,
+    balanceToy,
+    dinosaur,
+    educationalToy,
+    benchyShip,
+    snowballMaker,
+    shark,
+    frog,
+    legoToyParts,
+    toyScissors,
+    raptor,
+    tangramGame,
+    snake,
+    birdHouse,
+    birdFoodContainer,
+    automaticWaterCup,
+  },
 };
 
 export default Images;
